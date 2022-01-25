8a932b75cc697d9ad5ef1a9c2180379b1c791d8c
marunava21 <53409274+marunava21@users.noreply.github.com>
2022-01-25 15:20:32 +0800

    Update __init__.py

--- A2/__init__.py
+++ A2/__init__.py
@@ -1,6 +1,7 @@
+import logging
 from gym.envs.registration import register
 
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 register(
     id='A2Env-v0',
     entry_point='A2.env:A2Env',
