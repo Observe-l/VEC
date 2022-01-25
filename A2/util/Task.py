205e032d4f688e90f09d5fb9279480616829b096
jaimmmin <811649520@qq.com>
2022-01-25 21:23:28 +0800

    update table TASK and APIs

---
+++ A2/util/Task.py
@@ -0,0 +1,9 @@
+class Task():
+    def __init__(self):
+        self.id = None
+        self.offload_vehicle_id=None
+        self.service_vehicle_id=None
+        self.allocation_basestation_id=None
+        self.allocation_begin_time=None
+        self.allocation_end_time=None
+        self.done_status=None
---
+++ A2/util/TaskSQLUtil.py
@@ -0,0 +1,108 @@
+import datetime
+import pymysql
+from util.Task import Task
+import pandas as pd
+
+def insert(task:Task):
+    # conn = pymysql.connect(host='34.92.132.215', user='ray', passwd='Ray@123456', database='basestation')
+    conn = pymysql.connect(host='localhost', user='database', passwd='123456', database='basestation')
+    cursor = conn.cursor()
+    sql = "INSERT INTO TASK (id,offload_vehicle_id,\
+    service_vehicle_id,allocation_basestation_id,done_status) VALUES ("+str(task.id)+" ,"+str(task.offload_vehicle_id)+" ,"\
+              +str(task.service_vehicle_id)+" ,"+str(task.allocation_basestation_id)+" ,"\
+              +str(task.done_status)+");"
+
+    # 执行sql语句
+    cursor.execute(sql)
+    # 执行sql语句
+    conn.commit()
+
+    # 关闭数据库连接
+    conn.close()
+    return
+
+def countAllByBS(basestation_id):
+    # conn = pymysql.connect(host='localhost',port=3307,user='root',password='root',db='VEC.db')
+    conn = pymysql.connect(host='localhost', user='database', passwd='123456', database='basestation')
+    c = conn.cursor()
+    command = "SELECT * FROM TASK where allocation_basestation_id="+str(basestation_id)+";"
+    data = pd.read_sql(command, conn)
+    count=data.shape[0]
+    conn.close()
+    # print("select data successfully")
+    return count
+
+def countDoneByBS(basestation_id):
+    conn = pymysql.connect(host='localhost', user='database', passwd='123456', database='basestation')
+    c = conn.cursor()
+    command = "SELECT * FROM TASK where allocation_basestation_id=" + str(basestation_id) + " and done_status=1;"
+    data = pd.read_sql(command, conn)
+    count = data.shape[0]
+    conn.close()
+    # print("select data successfully")
+    return count
+
+
+def deleteAll():
+    # conn = pymysql.connect(host='34.92.132.215', user='ray', passwd='Ray@123456', database='basestation')
+    conn = pymysql.connect(host='localhost', user='database', passwd='123456', database='basestation')
+    c = conn.cursor()
+    command = "DELETE from TASK"
+    c.execute(command)
+    conn.commit()
+    conn.close()
+    # print("delete the table data")
+    return
+
+
+def createDB():
+    conn = pymysql.connect(host='localhost', user='database', passwd='123456', database='basestation')
+    c=conn.cursor()
+    command = "CREATE TABLE TASK( id VARCHAR(20) PRIMARY KEY NOT NULL,\
+            offload_vehicle_id REAL,\
+            service_vehicle_id REAL,\
+            allocation_basestation_id REAL,\
+            allocation_begin_time TIMESTAMP DEFAULT '2021-01-01 00:00:01',\
+            allocation_end_time TIMESTAMP DEFAULT '2022-01-01 00:00:01',\
+            done_status TINYINT);"
+    cursor=c.execute(command)
+    conn.commit()
+    conn.close()
+    print("create database successfully")
+    return
+
+
+def getNowTimestamp():
+    now=datetime.datetime.now()
+    now=now.strftime("%Y-%m-%d %H:%M:%S")
+    return now
+
+
+
+if __name__ == '__main__':
+    # createDB()
+
+    #load 2 tasks
+    # deleteAll()
+    # task1=Task()
+    # task1.id=1
+    # task1.offload_vehicle_id=1
+    # task1.allocation_begin_time=getNowTimestamp()
+    # task1.allocation_end_time=getNowTimestamp()
+    # task1.done_status=1
+    # task1.allocation_basestation_id=1
+    # task1.service_vehicle_id=2
+    #
+    # task2=Task()
+    # task2.id=4
+    # task2.offload_vehicle_id = 2
+    # task2.allocation_begin_time = getNowTimestamp()
+    # task2.allocation_end_time = getNowTimestamp()
+    # task2.done_status = 0
+    # task2.allocation_basestation_id = 2
+    # task2.service_vehicle_id = 1
+    # # insert(task1)
+    # insert(task2)
+
+    temp=countAllByBS(2)
+    print(temp)
