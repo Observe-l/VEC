6b5415a78062b8d7e2eeeb081f3ab3a7c52afa0a
marunava21 <53409274+marunava21@users.noreply.github.com>
2022-01-25 14:25:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 HERE = pathlib.Path(__file__).parent
 setup(name='A2',
       version='0.0.1',
+      url= 'https://github.com/Observe-l/VEC.git',
       install_requires=['gym>=0.2.3',
                         'hfo_py>=0.2']
 )
